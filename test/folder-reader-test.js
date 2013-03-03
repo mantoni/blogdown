f955490604ad6abe56ab5a9526d8e67dae9ccce3
Maximilian Antoni <mail@maxantoni.de>
2013-03-03 21:06:31 +0100

    Added folder-reader.readFolders implementation

--- test/folder-reader-test.js
+++ test/folder-reader-test.js
@@ -50,3 +50,58 @@ test('folder-reader readFiles', {
   }
 
 });
+
+
+test('folder-reader readFolders', {
+
+  before: function () {
+    sinon.stub(fs, 'readdir');
+    sinon.stub(fs, 'stat').yields(null, {
+      isDirectory : sinon.stub().returns(false)
+    });
+  },
+
+  after: function () {
+    fs.readdir.restore();
+    fs.stat.restore();
+  },
+
+  'invokes given callback with folder names': function () {
+    fs.readdir.withArgs('some/test').yields(null, ['a', 'b', 'c']);
+    fs.stat.withArgs('b').yields(null, {
+      isDirectory : sinon.stub().returns(true)
+    });
+    var spy = sinon.spy();
+
+    folderReader.readFolders('some/test', spy);
+
+    sinon.assert.calledOnce(spy);
+    sinon.assert.calledWith(spy, null, ['b']);
+  },
+
+
+  'yields error if readdir fails': function () {
+    var err = new Error('Oups!');
+    fs.readdir.withArgs('some/failure').yields(err);
+    var spy = sinon.spy();
+
+    folderReader.readFolders('some/failure', spy);
+
+    sinon.assert.calledOnce(spy);
+    sinon.assert.calledWith(spy, err);
+  },
+
+
+  'yields error if stat fails': function () {
+    fs.readdir.withArgs('some/test').yields(null, ['a', 'b', 'c']);
+    var err = new Error('Oups!');
+    fs.stat.withArgs('b').yields(err);
+    var spy = sinon.spy();
+
+    folderReader.readFolders('some/test', spy);
+
+    sinon.assert.calledOnce(spy);
+    sinon.assert.calledWith(spy, err);
+  }
+
+});
